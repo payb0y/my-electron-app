a24d4c96772f75af88ec57df9c60be6280f59860
Othmane Gattouchi <othmane.gattouchi@hps-worldwide.com>
2024-04-01 12:13:35 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+    const mainWindow = new BrowserWindow({ width: 800, height: 600 });
+    mainWindow.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
